e7b13f7c5cfd3fd3eac19ab543aaf3519177cc37
John Scardino <jscardino@jscardino-ltm1.internal.salesforce.com>
2022-09-06 10:36:10 -0400

    Updated Contact View for GovCloud

--- force-app/main/default/lwc/caseContactViewComponent/caseContactViewComponent.js
+++ force-app/main/default/lwc/caseContactViewComponent/caseContactViewComponent.js
@@ -1,13 +1,23 @@
 import { LightningElement, api, wire } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
-import CASE_CONTACT_ID from '@salesforce/schema/Case.ContactId';
-import CASE_CONTACT_NAME from '@salesforce/schema/Case.Contact.Name';
+import CASE_NUMBER from '@salesforce/schema/Case.CaseNumber';
+import CASE_CONTACT from '@salesforce/schema/Case.ContactId';
 import CONTACT_NAME from '@salesforce/schema/Contact.Name';
 import CONTACT_EMAIL from '@salesforce/schema/Contact.Email';
 import CONTACT_PHONE from '@salesforce/schema/Contact.Phone';
+import CONTACT_MOBILE from '@salesforce/schema/Contact.MobilePhone';
+import CONTACT_DODID from '@salesforce/schema/Contact.DODID__c';
+import CONTACT_MAILING_ADDRESS from '@salesforce/schema/Contact.MailingAddress';
+import CONTACT_RATE from '@salesforce/schema/Contact.Rate__c';
+import CONTACT_PAYGRADE from '@salesforce/schema/Contact.Paygrade__c';
+import CONTACT_SSN from '@salesforce/schema/Contact.SSN__c';
+import CONTACT_UIC from '@salesforce/schema/Contact.UIC__c';
+import CONTACT_ACTIVITY_NAME from '@salesforce/schema/Contact.Activity_Name__c';
+
+const WIRE_FIELDS = [ CASE_NUMBER, CASE_CONTACT ];
 
 export default class CaseContactViewComponent extends LightningElement {
-    fields = [CONTACT_NAME, CONTACT_EMAIL, CONTACT_PHONE];
+    contactFields = [CONTACT_NAME, CONTACT_PHONE, CONTACT_DODID, CONTACT_MOBILE, CONTACT_MAILING_ADDRESS, CONTACT_EMAIL, CONTACT_RATE, CONTACT_SSN, CONTACT_PAYGRADE, CONTACT_UIC, CONTACT_ACTIVITY_NAME];
 
     @api recordId;
     @api objectApiName;
@@ -15,7 +25,7 @@ export default class CaseContactViewComponent extends LightningElement {
     error = null;
     showContact = true;
 
-    @wire(getRecord, { recordId: '$recordId', fields: [CASE_CONTACT_ID, CASE_CONTACT_NAME] })
+    @wire(getRecord, { recordId: '$recordId', fields: WIRE_FIELDS })
     case({ error, data }) {
         if (data) {
             this.contactId = data.fields.ContactId.value;
